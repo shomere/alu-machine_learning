2407d9d8dc994620a79267f26524281ecc79f4e7
shomere <s.homere@alustudent.com>
2026-02-04 12:59:12 +0200

    11-the_western_exchange.py

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 """
-Module that provides a function to transpose a matrix (list of lists)
-without loops or imports.
+Module that provides a function to transpose a NumPy array.
 """
+
+import numpy as np
 
 
 def np_transpose(matrix):
     """
-    Returns the transpose of a 1D or 2D matrix.
+    Transposes the given NumPy array.
 
     Args:
-        matrix (list): Input 1D or 2D list.
+        matrix (numpy.ndarray): Input array to transpose.
 
     Returns:
-        list: Transposed list.
+        numpy.ndarray: Transposed array.
     """
-    if not matrix or not isinstance(matrix[0], list):
-        return matrix
-    return list(map(list, zip(*matrix)))
+    return np.transpose(matrix)
